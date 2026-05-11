002a3e4baeb0762321c8fbd36335019180ab8313
unknown <tusharbinjola85@gmail.com>
2026-05-11 08:29:35 -0700

    custom admin panel

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.models import User
+
+from .models import FileUpload
+
+
+# FILE UPLOAD ADMIN
+
+class FileUploadAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'user', 'file')
+
+    search_fields = ('user__username', 'file')
+
+    list_filter = ('user',)
+
+
+# REGISTER MODELS
+
+admin.site.register(FileUpload, FileUploadAdmin)
