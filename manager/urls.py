6b9681808136eb988eb6d6597d17dac3014000c6
unknown <tusharbinjola85@gmail.com>
2026-05-12 02:55:13 -0700

    added forgot password

--- manager/urls.py
+++ manager/urls.py
@@ -1,7 +1,39 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path(
+    'password_reset/',
+    auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html'
+    ),
+    name='password_reset'
+),
+
+path(
+    'password_reset_done/',
+    auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'
+    ),
+    name='password_reset_done'
+),
+
+path(
+    'reset/<uidb64>/<token>/',
+    auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html'
+    ),
+    name='password_reset_confirm'
+),
+
+path(
+    'reset_done/',
+    auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'
+    ),
+    name='password_reset_complete'
+),
     
     path('', views.login_view, name='login'),
 
@@ -10,7 +42,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
 
     path('share/<int:file_id>/', views.share_file, name='share_file'),
-    
+
     path('share/<int:file_id>/', views.share_file, name='share_file'),
 
     path('delete/<int:file_id>/', views.delete_file),
--- securecloud/settings.py
+++ securecloud/settings.py
@@ -127,3 +127,14 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'YOUR_GMAIL@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'anpx jtqf nime tbep'
